5f50ee1389da5b46521c347161862e5df46b337a
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-07 01:35:22 -0700

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -3,6 +3,7 @@ class Calculator:
     result = 0
     # Addition of two numbers
     def add(self, num1_a, num2_b):
+        """ Function to add two numbers """
         self.a =  num1_a
         self.result = self.a + num2_b
         return self.result
@@ -10,6 +11,7 @@ class Calculator:
 
     # Subtraction of two numbers
     def subtract(self, num1_a, num2_b):
+        """ Function to subtract two numbers """
         self.a = num1_a
         self.result = self.a - num2_b
         return self.result
@@ -17,12 +19,14 @@ class Calculator:
 
     # Multiplication of two numbers
     def multiply(self, num1_a, num2_b):
+        """ Function to multiply two numbers """
         self.a = num1_a
         self.result = self.a * num2_b
         return self.result
 
     # Division of numbers and exception for division by 0
     def divide(self, num1_a, num2_b):
+        """ Function to divide two numbers """
         self.a = num1_a
         if b <= 0:
             return "Error: A number cannot be divided by 0"
@@ -32,6 +36,7 @@ class Calculator:
 
     # Increment a number by 1
     def increment(self, num1_a):
+        """ Function to increment a number by 1 """
         self.a = num1_a
         self.result = self.a + 1
         return self.result
