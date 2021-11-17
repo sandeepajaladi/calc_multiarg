dc34dbe78a357c4b3bc1ab16faf474f5aac1f1c4
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-17 09:40:23 -0500

    Update test for division

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -24,9 +24,17 @@ def test_calculator_subtract_static(clear_history_fixture):
     assert Calculator.get_result_value() == -6.0
 
 def test_calculator_multiply_static(clear_history_fixture):
-    """Testing the subtract method of the calc"""
+    """Testing the multiplication method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
     #using Tuple instead of args because we can pack as much data as we need into the tuple
     my_tuple = (1.0,2.0,3.0)
     Calculator.multiply_numbers(my_tuple)
     assert Calculator.get_result_value() == 6.0
+    
+    def test_calculator_division_static(clear_history_fixture):
+    """Testing the division method of the calc"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    #using Tuple instead of args because we can pack as much data as we need into the tuple
+    my_tuple = (8.0,4.0,2.0)
+    Calculator.division_numbers(my_tuple)
+    assert Calculator.get_result_value() == 1.0
