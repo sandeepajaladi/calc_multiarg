ec540961f63babecf7903959318f098e90d86e5f
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-16 19:14:11 -0500

    Update division_test.py

--- tests/division_test.py
+++ tests/division_test.py
@@ -4,8 +4,8 @@ from calc.calculations.division import Division
 def test_calculation_division():
     """testing that our calculator has a static method for division"""
     #Arrange
-    mynumbers = (4.0,2.0)
+    mynumbers = (2.0,2.0)
     division = Division(mynumbers)
     #Act
     #Assert
-    assert division.get_result() == 2
+    assert division.get_result() == 1
