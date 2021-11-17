07bbcf59935c53a2805cd4e3acfe9adf27bfafd5
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-16 23:16:19 -0500

    Update division.py

--- calc/calculations/division.py
+++ calc/calculations/division.py
@@ -3,12 +3,9 @@ from calc.calculations.calculation import Calculation
 
 class Division(Calculation):
     """division calculation object"""
-    def get_result(self,num1,num2):
+    def get_result(self):
         """get the division results"""
-        result = 1.0
-        self.num_a = num1
-	    self.num_b = num2
         for value in self.values:
-            self.result = self.num_a / num_b
-        return self.result
+            result = result / value
+        return result
         
