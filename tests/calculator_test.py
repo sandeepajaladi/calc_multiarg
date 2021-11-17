c2a86e6a3587717a35be6c9ea3254caf707b0e36
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-17 11:15:11 -0500

    Update calculator_test.py

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -22,7 +22,6 @@ def test_calculator_subtract_static(clear_history_fixture):
     my_tuple = (1.0,2.0,3.0)
     Calculator.subtract_numbers(my_tuple)
     assert Calculator.get_result_value() == -6.0
-
 def test_calculator_multiply_static(clear_history_fixture):
     """Testing the multiplication method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
@@ -30,7 +29,6 @@ def test_calculator_multiply_static(clear_history_fixture):
     my_tuple = (1.0,2.0,3.0)
     Calculator.multiply_numbers(my_tuple)
     assert Calculator.get_result_value() == 6.0
-    
 def test_calculator_division_static(clear_history_fixture):
     """Testing the division method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
